46275ac6d8f9af65a196b15cdcf09591154fd96f
Ghhhhhhhj <pr.polis.p@gmail.com>
2025-02-05 23:34:05 +0100

    Finished functions: result, winner, terminal, utility

--- PS0/tictactoe/tictactoe.py
+++ PS0/tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -44,28 +45,52 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    if action not in actions(board):
+        raise ValueError("Invalid move")
+    copy_board = copy.deepcopy(board)
+    i, j = action[0], action[1]
+    copy_board[i][j] = player(board)
+    return copy_board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0]:
+            return board[i][0]
+    for i in range(3):
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i]:
+            return board[0][i]
+    if board[0][0] == board[1][1] == board [2][2] and board[0][0]:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2]:
+        return board[0][2]
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board):
+        return True
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == None:
+                return False
+    return True
+
 
 
 def utility(board):
-    """
-    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
-    """
-    raise NotImplementedError
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
